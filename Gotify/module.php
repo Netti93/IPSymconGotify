0bda4daab3d7e2fb0ac2511df7f5949d2cb8403c
Netti93 <nett.dominik@web.de>
2022-02-18 00:57:04 +0100

    Version 1.0 (#1)

--- Gotify/module.php
+++ Gotify/module.php
@@ -24,6 +24,14 @@ declare(strict_types=1);
 			parent::ApplyChanges();
 		}
 
+		private function BuildMessageURL() {
+			return rtrim($this->ReadPropertyString('URL'), '/') . '/message?token=' . $this->ReadPropertyString('ApplicationToken');
+		}
+		
+		public function SendTestMessage() {
+			return $this->SendMessageWithExtras($this->Translate('Test message'), $this->Translate('This is a test message from your Symcon instance'));
+		}
+
 		public function SendMessage(string $title, string $message, int $priority = 0)
         {
             return $this->SendMessageWithExtras($title, $message, $priority);
@@ -32,9 +40,8 @@ declare(strict_types=1);
 		public function SendMessageWithExtras(string $title, string $message, int $priority = 0, array $extras = [])
         {
             curl_setopt_array($ch = curl_init(), [
-                CURLOPT_URL        => $this->ReadPropertyString('URL'),
+                CURLOPT_URL        => $this->BuildMessageURL(),
                 CURLOPT_POSTFIELDS => [
-                    'token'     => $this->ReadPropertyString('ApplicationToken'),
                     'title'     => $title,
                     'message'   => $message,
 					'priority'  => $priority,
@@ -43,21 +50,33 @@ declare(strict_types=1);
                 CURLOPT_SAFE_UPLOAD    => true,
                 CURLOPT_RETURNTRANSFER => true,
             ]);
-            $response = curl_exec($ch);
+			$response = curl_exec($ch);
+			
+			// Check for errors and display the error message
+			if(!$response) {
+				$errorArr = array(
+					"error" => curl_strerror(curl_errno($ch)),
+					"errorCode" => curl_getinfo($ch, CURLINFO_RESPONSE_CODE),
+					"errorDescription" => curl_error($ch)
+				);
+				$this->LogMessage(json_encode($errorArr), KL_ERROR);
+                $this->SetStatus(201);
+                return false;
+			}
+
             curl_close($ch);
 
             $responseObject = json_decode($response);
-            if ($responseObject == null) {				
-                $this->SetStatus(201);
-                return false;
-            } else if (property_exists($responseObject, 'appid')) {
+			if (property_exists($responseObject, 'appid')) {
                 $this->SetStatus(102);
                 return true;
-            } else if (property_exists($responseObject, 'errorCode') && $responseObject->{'errorCode'} == 401) {
+            } else if (property_exists($responseObject, 'errorCode') && $responseObject->{'errorCode'} == 404) {
                 $this->SetStatus(202);
+            } else if (property_exists($responseObject, 'errorCode') && $responseObject->{'errorCode'} == 401) {
+                $this->SetStatus(203);
             }
 
-            IPS_LogMessage('Gotify', $response);
+            $this->LogMessage($response, KL_ERROR);
 
             return false;
         }
--- README.md
+++ README.md
@@ -1,6 +1,62 @@
 # Gotify
 
-Folgende Module beinhaltet das Gotify Repository:
+[![IPS-Version](https://img.shields.io/badge/Symcon_Version-5.3+-red.svg)](https://www.symcon.de/service/dokumentation/entwicklerbereich/sdk-tools/sdk-php/)
+![Code](https://img.shields.io/badge/Code-PHP-blue.svg)
+[![License](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-green.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)
 
-- __Gotify__ ([Dokumentation](Gotify))  
-	Kurze Beschreibung des Moduls.
+### Inhaltsverzeichnis
+
+- [Gotify](#gotify)
+		- [Inhaltsverzeichnis](#inhaltsverzeichnis)
+		- [1. Funktionsumfang](#1-funktionsumfang)
+		- [2. Vorraussetzungen](#2-vorraussetzungen)
+		- [3. Software-Installation](#3-software-installation)
+		- [4. Einrichten der Instanzen in IP-Symcon](#4-einrichten-der-instanzen-in-ip-symcon)
+		- [5. Konfiguration](#5-konfiguration)
+		- [6. PHP-Befehlsreferenz](#6-php-befehlsreferenz)
+		- [7. Anhang](#7-anhang)
+		- [8. Versionshistorie](#8-versionshistorie)
+
+### 1. Funktionsumfang
+
+* Das Modul ermöglicht das Senden von Benachrichtigungen via [Gotify](https://gotify.net).
+
+### 2. Vorraussetzungen
+
+- IP-Symcon ab Version 6.0
+- Ein von der IP-Symcon Instanz aus erreichbarer Gotify-Server
+- Ein App-Token von selbigem Gotify-Server
+
+### 3. Software-Installation
+
+* Über den Module Store das 'Gotify'-Modul installieren.
+* Alternativ über das Module Control folgende URL hinzufügen
+
+### 4. Einrichten der Instanzen in IP-Symcon
+
+ Unter 'Instanz hinzufügen' kann das 'Gotify'-Modul mithilfe des Schnellfilters gefunden werden.  
+	- Weitere Informationen zum Hinzufügen von Instanzen in der [Dokumentation der Instanzen](https://www.symcon.de/service/dokumentation/konzepte/instanzen/#Instanz_hinzufügen)
+
+### 5. Konfiguration
+
+siehe [Modulbeschreibung](Gotify/README.md#1-konfiguration)
+
+### 6. PHP-Befehlsreferenz
+
+siehe [Modulbeschreibung](Gotify/README.md#2-funktionsreferenz)
+
+### 7. Anhang
+
+GUIDs
+
+- Bibliothek: `{23DF70BD-570B-4CA9-1B85-4124A62C10DB}`
+- Module:
+  - Gotify: `{CB54965F-FAF7-A902-F9CA-606AB0614458}`
+
+Verweise:
+- https://gotify.net
+
+### 8. Versionshistorie
+
+- 1.0 @ 18.02.2022 00:50
+  - Initiale Version
